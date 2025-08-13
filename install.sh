7cbbf04096cb3e20710baf4c6853537be1fb1b3d
Felix Kimmel <felix.kimmel@web.de>
2025-08-13 10:43:27 +0200

    added automatic installation of deps

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install --no-install-recommends python3-pil ffmpeg
 mkdir -p /opt/copyparty/files/private
 cp copyparty.service /etc/systemd/system/copyparty.service
 systemctl daemon-reload
