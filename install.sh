454ec7370f48617894cf34b757874bb4e0ae62ce
Felix Kimmel <91203390+TheRobot2105@users.noreply.github.com>
2025-10-29 10:09:29 +0100

    Download and rename copyparty-sfx.py in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt install --no-install-recommends python3-pil ffmpeg
 mkdir -p /opt/copyparty/files/private
+wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py
+mv -f copyparty-sfx.py copyparty.py
 cp copyparty.service /etc/systemd/system/copyparty.service
 systemctl daemon-reload
 systemctl enable copyparty
